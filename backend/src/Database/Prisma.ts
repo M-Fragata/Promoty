c59f29db7816e91c9c9fc0987713fe300f77311c
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-05-29 16:50:48 -0300

    Update Prisma.ts

--- backend/src/Database/Prisma.ts
+++ backend/src/Database/Prisma.ts
@@ -2,7 +2,9 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+import { Env } from "../utils/Envirolment.js";
+
+const connectionString = `${Env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });

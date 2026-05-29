45efac6e4c4524e687ccf2607f457c9e6681a2cd
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-05-29 16:52:06 -0300

    arquivo utils para verificação das variáveis

---
+++ backend/src/utils/Envirolment.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod'
+import 'dotenv/config'
+
+const EnvSchema = z.object({
+    PORT: z.string().default('3333'),
+    AMAZON_TAG: z.string(),
+    SHOPEE_ID: z.string().default(''),
+    DATABASE_URL: z.string(),
+    WHATSAPP_GROUP_JID: z.string(),
+    TELEGRAM_API_ID: z.coerce.number(),
+    TELEGRAM_API_HASH: z.string(),
+    AWIN_PUBLISHER_ID: z.string(),
+    TELEGRAM_KABUM_ID: z.string(),
+})
+
+const Envirol = EnvSchema.safeParse(process.env);
+
+if (!Envirol.success) {
+    console.error("❌ Erro crítico nas variáveis de ambiente (.env):");
+    // Formata os erros do Zod para mostrar exatamente qual campo quebrou
+    console.error(Envirol.error.format());
+    process.exit(1);
+}
+
+// Exporta as variáveis perfeitamente tipadas (e com o API_ID já como número puro!)
+export const Env = Envirol.data;

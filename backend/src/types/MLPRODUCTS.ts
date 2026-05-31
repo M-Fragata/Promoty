9ef4703762b6845d21158cc28b10b94210226ccc
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-05-31 12:25:25 -0300

    interface de produtos da tabela

---
+++ backend/src/types/MLPRODUCTS.ts
@@ -0,0 +1,11 @@
+export interface MlProducts {
+    id: string;
+    title: string;
+    price: number;
+    originalPrice: number | null;
+    coupon: string | null;
+    badge: string | null;
+    imageUrl: string | null;
+    link: string;
+    store: string;
+}

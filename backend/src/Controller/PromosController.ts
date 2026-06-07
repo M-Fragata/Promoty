9f1489082cf4c5ad60508efc3f3a7f0ef6ac9503
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-06-07 20:45:05 -0300

    adicionando linha no parcelamento

--- backend/src/Controller/PromosController.ts
+++ backend/src/Controller/PromosController.ts
@@ -37,6 +37,8 @@ export class PromosController {
             lines.push(`~De: R$ ${product.originalPrice.toFixed(2)}~`);
         }
 
+        lines.push('')
+
         lines.push(`Por: R$ *${product.price.toFixed(2)}*`);
         if(product.installments){
             lines.push(`💳 ${product.installments}`)

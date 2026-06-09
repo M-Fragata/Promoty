c3646612e2b5a6b9ea4dee170f2e85022f1f6abb
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-06-09 20:33:45 -0300

    Update OffersRoutes.ts

--- backend/src/Routes/OffersRoutes.ts
+++ backend/src/Routes/OffersRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import { PromosController } from '../Controller/PromosController.js';
+import { ShopeePromosController } from "../Controller/ShopeePromosController.js"
 
 export const offersRoutes = Router();
 const promosController = new PromosController();
+const shopeePromoController = new ShopeePromosController()
 
 offersRoutes.post('/mercadolivre', promosController.processProductsML);
 offersRoutes.post('/amazon', promosController.processProductsAmazon);
+offersRoutes.post('/shopee', shopeePromoController.GetProducts)
+offersRoutes.post('/shopee/info', shopeePromoController.GetInfo)

304c78112a361a6542664388d757009926d7d5f5
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-06-14 19:51:41 -0300

    arquivo para unir os grupos de cada nicho

---
+++ backend/src/config/index.ts
@@ -0,0 +1,30 @@
+import type { NicheConfig } from "../types/niche.js";
+import { Env } from "../utils/Envirolment.js";
+import { techNiche } from "./tech.niche.js";
+import { modaFeminina } from "./moda-feminina.niche.js";
+
+let cachedNiches: NicheConfig[] | null = null;
+
+export function getActiveNiches(): NicheConfig[] {
+  if (cachedNiches) return cachedNiches;
+
+  const niches: NicheConfig[] = [];
+
+  niches.push({
+    ...techNiche,
+    groupJid: Env.WHATSAPP_GROUP_JID,
+  });
+
+  niches.push({
+    ...modaFeminina,
+    groupJid: Env.WHATSAPP_GROUP_JID_MODA_FEMININA,
+    groupInviteLink: Env.WHATSAPP_GROUP_INVITE_MODA_FEMININA,
+  });
+
+  cachedNiches = niches;
+  return niches;
+}
+
+export function getNicheById(id: string): NicheConfig | undefined {
+  return getActiveNiches().find((n) => n.id === id);
+}
